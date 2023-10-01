f8345e19cbb7394fb6f83d6f1ac4c28cbec874fd
Bruno Miguel Fonseca Cavaca (k22017608) <bruno.fonseca_cavaca@kcl.ac.uk>
2023-10-01 18:58:59 +0100

    Implemented Function

--- primes.py
+++ primes.py
@@ -3,4 +3,10 @@
 
 def primes(number_of_primes):
     list = []
+    start = 2
+    while (list.length()!=number_of_primes):
+        for value in range(2,int(start**0.5)+1):
+            if (start % value != 0):
+                list.append(start)
+        start+=1
     return list
