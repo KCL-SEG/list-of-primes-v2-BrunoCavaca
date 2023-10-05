df3cf3f213b7af55860897a2c6cec16bbba6455c
Bruno Miguel Fonseca Cavaca (k22017608) <bruno.fonseca_cavaca@kcl.ac.uk>
2023-10-05 10:18:14 +0100

    Update to handle values less than or equal to 0

--- primes.py
+++ primes.py
@@ -4,16 +4,21 @@
 def primes(number_of_primes):
     prime = []
     start = 2
-    while len(prime) < number_of_primes:
-        primeTest = []
-        for i in prime:
-            print(i)
-            if start%i==0:
-                print("pass")
-                primeTest.append(start)
-        if primeTest:
-            prime += []
-        else:
-            prime += [start]
-        start += 1
-    return prime
+    if number_of_primes>0:
+        while len(prime) < number_of_primes:
+            primeTest = []
+            for i in prime:
+                print(i)
+                if start%i==0:
+                    print("pass")
+                    primeTest.append(start)
+            if primeTest:
+                prime += []
+            else:
+                prime += [start]
+            start += 1
+        return prime
+    else:
+        raise ValueError("Enter a valid value")
+
+print(primes(0))
